e3360c91e9db4d0206f9a18abd1778f86c60ed03
AkshayJayachandranVV <akshayjayachandranvv@gmail.com>
2025-04-07 11:15:43 +0530

    first

---
+++ dist/user/user.service.d.ts
@@ -0,0 +1,8 @@
+import { User } from './enitity/user.entity';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user-dto';
+export declare class UserService {
+    private readonly userRepository;
+    constructor(userRepository: Repository<User>);
+    createUser(createUserDto: CreateUserDto): Promise<User>;
+}
